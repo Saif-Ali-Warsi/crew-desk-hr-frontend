4b6e131cc13b3a2695cd66bf91dd707aa8945a8d
saif <warsivorlox@gmail.com>
2026-08-16 19:07:40 +0530

    fix github pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import { HashRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/crew-desk-hr-frontend">
+    <HashRouter>
       <AuthProvider>
-        <HashRouter>
-          <App />
-        </HashRouter>
+        <App />
         <ToastContainer position="top-right" autoClose={3000} />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
