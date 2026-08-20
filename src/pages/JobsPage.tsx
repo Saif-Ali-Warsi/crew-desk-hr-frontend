ad78fe778dc8f86be8a155dfb4412871502a7d03
saif <warsivorlox@gmail.com>
2026-08-20 20:27:51 +0530

    ui improvements and sidebar fixed

--- src/pages/JobsPage.tsx
+++ src/pages/JobsPage.tsx
@@ -240,7 +240,7 @@ function JobsPage() {
         </div>
       )}
 
-      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="flex items-center justify-between py-3">
         <button
           disabled={!pagination?.hasPrevious}
           onClick={() => setPage((currentPage) => currentPage - 1)}
